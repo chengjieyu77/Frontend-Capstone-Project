b14c91dca8cd0af8ea1cdbd21434f9b254b5f997
chengjieyu77 <chengjieyu01@gmail.com/n>
2024-03-07 16:13:51 +0800

    improve booking function

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,10 +3,13 @@ import { useState } from "react";
 
 
 const BookingForm = (props) => {
-    const [date,setDate] = useState("");
-    const[time,setTime] = useState("");
-    const[guestsNum,setGusetsNum] = useState(1);
-    const[occasion,setOccasion] = useState("");
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName,setLastName] = useState("");
+    // const [contactNumber,setContactNumber] = useState(123-333-333);
+    // const [date,setDate] = useState("");
+    // const[time,setTime] = useState("");
+    // const[guestsNum,setGusetsNum] = useState(1);
+    // const[occasion,setOccasion] = useState("");
 
     const [formData, setFormData] = useState({
         firstName: "",
@@ -18,54 +21,52 @@ const BookingForm = (props) => {
         occasion: "Birthday"
     })
 
+    const handleFormChange = (event) => {
+        const { name, value } = event.target
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value
+        }))
+      }
 
-    const clearForm = () => {
-        setDate("");
-        setTime("");
-        setGusetsNum(1);
-        setOccasion("");
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.submitForm(formData)
-        clearForm();
-        // sendDataToBookingPage();
+        props.submitForm(formData);
     }
 
-    const sendDataToBookingPage=()=>{
-        props.getDateFromBookingForm(date);
-    }
     return(
         <div className="book-now">
         <h1 className="form-title">Book Now</h1>
         <form onSubmit={handleSubmit}>
             <fieldset>
-            <label htmlFor="res-date">Choose date</label>
-                <input type="date" id="res-date" value={date} onChange={(e) => {setDate(e.target.value)}} required/>
-            
-            
+                <label htmlFor="first-name">First name: </label>
+                <input type="text" id="first-name" name="firstName" value={formData.firstName} onChange={handleFormChange} required/>
+                <label htmlFor="last-name">Last name: </label>
+                <input type="text" id="lastfirst-name" name="lastName" value={formData.lastName} onChange={handleFormChange} required/>
+                <label htmlFor="contact-number">Contact Number: </label>
+                <input type="number" id="contact-number" name="contactNumber" value={formData.contactNumber} onChange={handleFormChange} required placeholder="123-456-789"  pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"/>
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" name="date" value={formData.date} onChange={handleFormChange} required/>
                 <label htmlFor="res-time"  >Choose time</label>
-                <select id="res-time " value={time} onChange={(e) => {setTime(e.target.value)}} required>
+                <select id="res-time " name="time" value={formData.time} onChange={handleFormChange} required>
 
                     {props.availableTimes.map((availableTime) => {
                         return <option>{availableTime}</option>
                     })}
                 </select>
                 <label htmlFor="guests">Number of guests</label>
-                <input type="number" placeholder="1" min="1" max="10" id="guests" value={guestsNum} onChange={(e) => {setGusetsNum(e.target.value)}} required/>
+                <input type="number" placeholder="1" min="1" max="10" id="guests" name="nOfGuests" value={formData.noOfGuests} onChange={handleFormChange} required/>
                 <label htmlFor="occasion">Occasion</label>
-                <select id="occasion" value={occasion} onChange={(e) => {setOccasion(e.target.value)}}>
+                <select id="occasion" name="occasion" value={formData.occasion} onChange={handleFormChange}>
                     <option>Birthday</option>
                     <option>Anniversary</option>
                     <option>other</option>
                 </select>
             </fieldset>
-                
             <div className="form-button-container">
             <input type="submit" value="Make Your reservation" className="button" aria-label="On Click"/>
             </div>
-            
         </form>
         </div>
     )
